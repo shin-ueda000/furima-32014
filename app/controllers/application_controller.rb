268c3848d5758edcc90947da90a51b532716d727
shin-ueda000 <73635333+shin-ueda000@users.noreply.github.com>
2020-11-24 10:02:09 +0900

    テストコード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def 
+    configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_katakana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_katakana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+         validates :nickname
+         validates :password, format:    { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "please include both letters and numbers in."}
+         validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Input in full-width (kanji, hiragana, katakana) is required"}
+         validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Input in full-width (kanji, hiragana, katakana) is required"}
+         validates :family_name_katakana,  format: { with:/\A[ァ-ヶー－]+\z/, message: "Frigana must be entered in full-width (katakana)"}
+         validates :first_name_katakana,   format: { with:/\A[ァ-ヶー－]+\z/, message: "Frigana must be entered in full-width (katakana)"}
+         validates :birthday
+         end
+end
