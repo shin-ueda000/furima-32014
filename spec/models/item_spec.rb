39a2f357646b4f69d96c49768f0f3d72ca18efcf
shin-ueda000 <73635333+shin-ueda000@users.noreply.github.com>
2020-11-26 12:03:38 +0900

    修正しました

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -43,10 +43,40 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
       end
-      it "発売元の地域についての情報ほうがid[1]では登録出来ない" do
+      it "発売元の地域についての情報がid[1]では登録出来ない" do
         @item.shipping_area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
+      end
+      it "発送までの日数についての情報がid[1]では登録出来ない" do
+        @item.days_to_sip_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to sip must be other than 1")
+      end
+      it "カテゴリーについての情報が空だと登録出来ない" do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+      end
+      it "商品の状態についての情報が空だと登録出来ない" do
+        @item.product_condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product condition can't be blank", "Product condition is not a number")
+      end
+      it "発送料の負担についての情報が空だと登録出来ない" do
+        @item.shipping_charge_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge can't be blank", "Shipping charge is not a number")
+      end
+      it "発送元の地域についての情報が空だと登録出来ない" do
+        @item.shipping_area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area can't be blank", "Shipping area is not a number")
+      end
+      it "発送日までの日数についての情報が空だと登録出来ない" do
+        @item.days_to_sip_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to sip can't be blank", "Days to sip is not a number")
       end
       it "価格についての情報が空だと登録出来ない" do
         @item.selling_price = nil
