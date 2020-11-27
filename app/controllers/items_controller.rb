c92f93d384556be3611e36423bb3f2d225d6cb90
shin-ueda000 <73635333+shin-ueda000@users.noreply.github.com>
2020-11-27 12:47:55 +0900

    投稿削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,6 +35,12 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
   private
 
   def item_params
