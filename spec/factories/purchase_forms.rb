ea865635a60113da70bd92de610e611425644821
shin-ueda000 <73635333+shin-ueda000@users.noreply.github.com>
2020-12-02 21:48:15 +0900

    テストコード の修正

--- spec/factories/purchase_forms.rb
+++ spec/factories/purchase_forms.rb
@@ -7,6 +7,5 @@ FactoryBot.define do
     building_name {'柳ビル103'}
     phone_number { '00000000000' }
     token {"tok_abcdefghijk00000000000000000"}
-
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              {"test"}
-    email                 {"test@example"}
+    email                 {Faker::Internet.email}
     password              {"a000000"}
     password_confirmation {password}
     family_name           {"山田"}
